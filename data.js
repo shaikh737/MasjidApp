320a394c4ed2088fc71906757aa1b18a1f7e63b0
Shaikh Hamza <125948842+shaikh737@users.noreply.github.com>
2025-06-14 16:21:33 -0600

    Add Data file
    Data.js
    json format

---
+++ data.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 0,
+    name: "Fajr",
+    place: "Madni Masjid",
+    time: "04:00 AM",
+  },
+  {
+    id: 1,
+    name: "Zuhr",
+    place: "Madni Masjid",
+    time: "01:03 PM",
+  },
+  {
+    id: 2,
+    name: "Asar",
+    place: "Madni Masjid",
+    time: "04:03 PM",
+  },
+  {
+    id: 3,
+    name: "Magrib",
+    place: "Madni Masjid",
+    time: "06:03 PM",
+  },
+  {
+    id: 4,
+    name: "Isha",
+    place: "Madni Masjid",
+    time: "07:43 PM",
+  },
+  {
+    id: 5,
+    name: "Jummah",
+    place: "Madni Masjid",
+    time: "01:30 PM",
+  },
+];
